8ef590a6a83313d457a5459da2f929e1f706aa3f
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-06-27 14:37:34 +0300

    Initial commit

--- src/main/scala/DayTwo.scala
+++ src/main/scala/DayTwo.scala
@@ -1,3 +1,9 @@
-object DayTwo extends App {
+object DayTwo {
+  def main(args:Array[String]): Unit = {
+    println("Temporary views") //Views let us avoid creating data collections in memory when performing multiple operations
+    // So this idea of Views is quite common across different data processing languages - we will see it again when we work with Spark
 
+    val numbers = (0 to 12).toArray
+    println(numbers.mkString(","))
+  }
 }
